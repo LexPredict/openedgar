23e10c115820f9b35867f9f59e29e2d13799f3c1
Mirko Bernardoni <mirko.bernardoni@gmail.com>
2019-05-09 10:25:04 +0100

    Added rate limit to 10/s Pipe for celery log

--- docker/run_edgar.py
+++ docker/run_edgar.py
@@ -1,5 +1,7 @@
+import logging
 import os
 from openedgar.processes.edgar import download_filing_index_data, process_all_filing_index
+
 
 year = os.getenv("EDGAR_YEAR")
 quarter = os.getenv("EDGAR_QUARTER")
@@ -16,7 +18,8 @@ types = [t.upper().strip() for t in os.getenv("FORM_TYPES").split(",")]
 #£
 # fix: openedgar.tasks: ERROR    Multiple Filing records found for s3_path=edgar/data/1096325/0001096325-15-000002.txt, skipping...
 # fix: openedgar.tasks: ERROR    Unable to create filing documents for Filing id=30186, cik=1024725, form_type=8-K, date_filed=2015-02-02: 'ascii' codec can't encode character '\xa0' in position 20: ordinal not in range(128)
-# Reduce log level
+print("Edgar analysis started")
+print("Analysing year {} types {}".format(year, types))
 process_all_filing_index(year=year, form_type_list=types)
 
 for i in range(1, 10):
