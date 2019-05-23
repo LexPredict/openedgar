d338484152b1bc3249385c291838a24cb033781a
Mirko Bernardoni <mirko.bernardoni@gmail.com>
2019-05-23 14:31:23 +0100

    Multiple years handling from variable

--- docker/run_edgar.py
+++ docker/run_edgar.py
@@ -2,7 +2,7 @@ import logging
 import os
 from openedgar.processes.edgar import download_filing_index_data, process_all_filing_index
 
-year = os.getenv("EDGAR_YEAR")
+years = [t.upper().strip() for t in os.getenv("EDGAR_YEAR").split(",")]
 quarter = os.getenv("EDGAR_QUARTER")
 month = os.getenv("EDGAR_MONTH")
 types = [t.upper().strip() for t in os.getenv("FORM_TYPES").split(",")]
@@ -13,8 +13,9 @@ if len(month) == 0:
     month = None
 
 print("Edgar analysis started")
-print("Analysing year {} types {}".format(year, types))
-process_all_filing_index(year=year, quarter=quarter, month=month, form_type_list=types)
+for year in years:
+    print("Analysing year {} types {}".format(year, types))
+    process_all_filing_index(year=year, quarter=quarter, month=month, form_type_list=types)
 
 for i in range(1, 10):
     print("###############################################")
